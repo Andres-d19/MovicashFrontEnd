d2b6f052f87890b7912398e9a676df5f341d9247
Zayda1995fer <zaydafervar@gmail.com>
2025-04-09 14:47:47 -0600

    cambios

---
+++ .angular/cache/18.2.17/client/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,28 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-ZD3ILK37.js";
+import "./chunk-45PCNLPI.js";
+import "./chunk-J7GMJMWH.js";
+import "./chunk-WFP5MTKJ.js";
+import "./chunk-5FH35E2S.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-WWJLZDDC.js";
+import "./chunk-5IW5ZEPE.js";
+import "./chunk-UOPINYA3.js";
+import "./chunk-RPWZ4CMX.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
